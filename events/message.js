b3fac893c1146ea12f055f0b39e2c276323d5fcb
Hayl <hayl0830@gmail.com>
2018-06-10 18:11:29 +1000

    .

--- events/message.js
+++ events/message.js
@@ -1,11 +1,9 @@
 module.exports = async (bot, message) => {
     if (message.channel.type === "dm" && message.author.id != bot.user.id){
-
       console.log("[DM] " + message.channel.recipient.username + " | " + message.content);
-
       channel_ID = '400779864191401984';
       guild_ID = '356764662760472576';
-      bot.guilds.get(guild_ID).channels.get(channel_ID).send(message.content + " [DM]" + message.author + " <@338163785082601473>");
+      bot.guilds.get(guild_ID).channels.get(channel_ID).send(message.content + " [DM]" + message.channel.recipient.username + " <@338163785082601473>");
 
     }
     if (message.channel.type === "dm") return;
