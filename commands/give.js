a84112daa68bafe5296366af7c32fef2ac51a9dd
Hayl <hayl0830@gmail.com>
2018-07-15 19:23:13 +1000

    fix give function

--- commands/give.js
+++ commands/give.js
@@ -1,5 +1,6 @@
 exports.run = (bot, message, args) => {
-  if (message.author.id != '338163785082601473' )return;
+  if(!message.member.hasPermission("MANAGE_ROLES"))
+    return message.reply( "No no no *YOU'RE* not allowed to do that! ");
   let member = message.mentions.members.first();
   var x = args.shift();
   const new_role = args.join(" ");
