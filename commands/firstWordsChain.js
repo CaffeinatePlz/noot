2427f9dffb08a0a045c88a4bdc8b71cc2668c440
Hayl <hayl0830@gmail.com>
2018-07-15 19:48:26 +1000

    hmmm

---
+++ commands/firstWordsChain.js
@@ -0,0 +1,21 @@
+exports.run = (bot, message, args) => {
+	const MarkovChain = require('markovchain');
+	const fs = require('fs');
+	const koalafacts = new MarkovChain(fs.readFileSync('./commands/frc.txt', 'utf8'));
+	message.channel.send(`${koalafacts.start("FRC").end(" ").process()}`);
+};
+
+
+exports.conf = {
+	enabled: true,
+	guildOnly: false,
+	aliases: ['frc', 'frcwords', 'whatisfrc'],
+	botPerms: [],
+	memberPerms: []
+};
+
+exports.help = {
+	name: 'frcChain',
+	description: 'A Markov Chain for FRC',
+	usage: 'frcChain'
+};
