de67544c7f340535975e10f6f49e6bda1e5cbec9
Hayl <hayl0830@gmail.com>
2018-08-15 21:29:30 +1000

    Train Game :)

--- commands/trainGame.js
+++ commands/trainGame.js
@@ -5,7 +5,6 @@ exports.run = (bot, message, args) => {
     n2 = parseInt(args[0].slice(1,2));
     n3 = parseInt(args[0].slice(2,3));
     n4 = parseInt(args[0].slice(3,4));
-    message.reply("solving with " + n1 + ", "+ n2 + ", "+ n3 + ", "+ n4);
     solve(n1, n2, n3, n4);
   } else {
     message.reply ("Please enter 4 numbers.");
@@ -50,39 +49,6 @@ exports.run = (bot, message, args) => {
       }
   }
 
-/*
-  function op(num1, num2, operation) {
-    if (operation == "+"){
-      return num1+num2;
-    }else if (operation == "-"){
-      return num1-num2;
-    }else if (operation == "*"){
-      return num1*num2;
-    }else if (operation == "/"){
-      return num1/num2;
-    }
-  }
-
-  function solve(a, b, c, d){
-    var symbols = ["+", "-", "*", "/"];
-    var solutions = "";
-    for (var s1 in symbols){
-      for (var s2 in symbols){
-          for (var s3 in symbols){
-            try {
-              var num = op(op(op(a, b, s1), c, s2), d, s3);
-              if (num == 10){
-                solutions += `((${a}${s1}${b})${s2}${c})${s3}${d}\n`;
-                message.channel.send("((" + a + s1 + b + ")" + s2 + c + ")" + s3 + d);
-              }
-            } catch (e) {
-          }
-        }
-      }
-    }
-    message.channel.send(solutions);
-  }
-*/
 
 
 };
