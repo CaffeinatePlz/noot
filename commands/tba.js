f4d634e49b29ba874a723a143d5f847a1bcaa054
Hayl <hayl0830@gmail.com>
2018-08-15 18:14:27 +1000

    .

--- commands/tba.js
+++ commands/tba.js
@@ -12,9 +12,9 @@ exports.run = (bot, message, args) => {
         .addField('Rookie Year', a.rookie_year, true)
         .addField('Location', `${a.city}, ${a.state_prov}, ${a.country}`, true)
         .addField('Website', a.website, true);
-      if (a.motto !== null) { teaminfo.addField('Motto', a.motto, true); };
+      //if (a.motto !== null) { teaminfo.addField('Motto', a.motto, true); };
       sendEmbed(info);
-    }).catch(e => { message.channel.sendMessage('I cannot find this team. Does it exist?'); });
+    }).catch(e => { message.channel.sendMessage('```js\n'+e+"```"); message.channel.sendMessage('I cannot find this team. Does it exist?'); });
   } else {
     message.channel.sendMessage('Please mention a team (`+tba <team_number>`)');
   }
