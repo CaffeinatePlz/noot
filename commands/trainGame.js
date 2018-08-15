4497aea04d4b57ce55d44e33c2bc9a1d2485a89f
Hayl <hayl0830@gmail.com>
2018-08-15 19:31:52 +1000

    hmmm

--- commands/trainGame.js
+++ commands/trainGame.js
@@ -1,5 +1,17 @@
 exports.run = (bot, message, args) => {
-	function op(num1, num2, operation) {
+  if (!isNaN(args[0]) && args[0].length ==4){
+    n1 = args[0].slice(0,1);
+    n2 = args[0].slice(1,2);
+    n3 = args[0].slice(2,3);
+    n4 = args[0].slice(3,4);
+    message.reply("solving with " + n1 ", "+ n2 ", "+ n3 ", "+ n4);
+    solve(n1, n2, n3, n4);
+  } else {
+    message.reply ("Please enter 4 numbers.");
+  }
+
+
+  function op(num1, num2, operation) {
     if (operation == "+"){
       return num1+num2;
     }else if (operation == "-"){
@@ -28,16 +40,7 @@ exports.run = (bot, message, args) => {
     }
   }
 
-  if (!isNaN(args[0]) && args[0].length ==4){
-    n1 = args[0].slice(0,1);
-    n2 = args[0].slice(1,2);
-    n3 = args[0].slice(2,3);
-    n4 = args[0].slice(3,4);
-    message.reply("solving with " + n1 ", "+ n2 ", "+ n3 ", "+ n4);
-    solve(n1, n2, n3, n4);
-  } else {
-    message.reply ("Please enter 4 numbers.");
-  }
+
 
 };
 
