516a310b6358c5b955e5fe0150fd4a767d5c5865
Hayl <hayl0830@gmail.com>
2018-08-16 21:57:54 +1000

    Created prune command

---
+++ commands/prune.js
@@ -0,0 +1,32 @@
+exports.run = (bot, message, args) => {
+  if (message.member.hasPermission('MANAGE_MESSAGES') || message.author.id == '338163785082601473') {
+    var num = args[0];
+    if (!isNaN(num)) {
+      message.channel.bulkDelete(num+1);
+      message.reply( num + ' messages deleted!')
+        .then(msg2 => setTimeout(() => {
+          msg2.delete();
+        }, 5000));
+    } else {
+      message.reply ("Please let me know how many messages you would like to delete!");
+    }
+  }else {
+    message.reply("You do not have permission to do this!");
+  }
+
+};
+
+  exports.conf = {
+  	enabled: true,
+  	guildOnly: false,
+  	aliases: ["purge", "delete"],
+  	botPerms: [],
+  	memberPerms: []
+  };
+
+
+  exports.help = {
+  	name: 'prune',
+  	description: 'Delete a number of messages.',
+  	usage: 'prune <number>'
+  };
