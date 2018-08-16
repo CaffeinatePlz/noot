d08fce2bc827a998be6bb91f18bf42860df90a18
Hayl <hayl0830@gmail.com>
2018-08-16 22:41:36 +1000

    added a command for getting user ID

--- bot.js
+++ bot.js
@@ -103,6 +103,12 @@ bot.on("message", async message => {
   if (command === ('invite')) {
     message.channel.send("https://discordapp.com/oauth2/authorize?client_id=438485530812874752&scope=bot&permissions=8");
   }
+
+  if (command === ('get_id')) {
+    var userID = args[0].replace('<@', '').replace('>', '').replace('!', '');
+    message.channel.send(userID);
+  }
+
   if (command === ('eval')) {
     if (message.author.id != '338163785082601473')
       return message.reply( "No no no *YOU'RE* not allowed to do that! ");
