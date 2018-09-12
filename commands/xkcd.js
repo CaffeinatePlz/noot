3b1b566a69fc8ca96a92853ad7955cee8232969d
Hayl <hayl0830@gmail.com>
2018-09-12 15:09:43 +1000

    .

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -23,17 +23,7 @@ exports.run = (bot, message, args) => {
     });
   } else if (!isNaN(args[0])){
     xkcd(args[0], function (data) {
-      try {
-        message.channel.send({ embed: new Discord.RichEmbed()
-          .setAuthor('XKCD #' + data.num)
-          .setTitle(data.safe_title)
-          .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
-          .setImage(data.img)
-          .setDescription(data.alt)
-        });
-      } catch (err) {
-        message.channel.send(err);
-      }
+
     });
   } else {
     message.reply("Please use +xkcd <number>");
