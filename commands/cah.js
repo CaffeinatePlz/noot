22bac998f108f6e924c95e0939008000b1e7bee7
Hayl <hayl0830@gmail.com>
2018-09-12 12:43:34 +1000

    .

--- commands/cah.js
+++ commands/cah.js
@@ -9,16 +9,16 @@ exports.run = (bot, message, args) => {
   let description = '';
   let author = '';
 
-  const body = request.get('http://www.amazingsuperpowers.com/?randomcomic&nocache=1');
-        const $ = cheerio.load(body);
-        img = $('#comic-1 img').attr('src');
-        link = $('.post-title a').attr('href');
-        title = $('.post-title a').text();
-        description = $('.entry').text();
-        author = 'AmazingSuperPowers';
+//  var x = Math.floor(Math.random() * 5035);
+  const body = request.get('http://explosm.net/comics/random');
+  const $ = cheerio.load(body);
+  img = $('#main-comic').attr('src').replace('//', 'http://');
+  link = $('#permalink').attr('value');
+  author = 'Cyanide and Happiness';
+
 
   message.channel.send({ embed: new Discord.RichEmbed()
-      .setAuthor(author)
+      .setAuthor(author, 'https://i.imgur.com/jJl7FoT.jpg')
       .setTitle(title)
       .setURL(link)
       .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
