0b89fba58559e55063929a29b1ff8b059bf4fa07
Hayl <hayl0830@gmail.com>
2018-09-12 15:14:08 +1000

    .

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -7,19 +7,21 @@ exports.run = (bot, message, args) => {
   let author = '';
 
   if (!args[0]){
-    xkcd(function (data) {
-
-      try {
-        message.channel.send({ embed: new Discord.RichEmbed()
-          .setAuthor('XKCD #' + data.num)
-          .setTitle(data.safe_title)
-          .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
-          .setImage(data.img)
-          .setDescription(data.alt)
-        });
-      } catch (err) {
-        message.channel.send(err);
-      }
+    xkcd(function (data1) {
+      let x = Math.floor(Math.random() * data1.num);
+      xkcd(x, function (data){
+        try {
+          message.channel.send({ embed: new Discord.RichEmbed()
+            .setAuthor('XKCD #' + data.num)
+            .setTitle(data.safe_title)
+            .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+            .setImage(data.img)
+            .setDescription(data.alt)
+          });
+        } catch (err) {
+          message.channel.send(err);
+        }
+      });
     });
   } else if (!isNaN(args[0])){
     xkcd(args[0], function (data) {
