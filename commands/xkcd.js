2f1ebfc8f9dc6271f20063a28d5da4608aa3672f
Hayl <hayl0830@gmail.com>
2018-09-12 15:06:51 +1000

    im dum

--- commands/xkcd.js
+++ commands/xkcd.js
@@ -8,22 +8,32 @@ exports.run = (bot, message, args) => {
 
   if (!args[0]){
     xkcd(function (data) {
-      message.channel.send({ embed: new Discord.RichEmbed()
-        .setAuthor('XKCD #' + data.num)
-        .setTitle(data.safe_title)
-        .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
-        .setImage(data.img)
-        .setDescription(data.alt)
-      });
-      /*try {
 
+      try {
+        message.channel.send({ embed: new Discord.RichEmbed()
+          .setAuthor('XKCD #' + data.num)
+          .setTitle(data.safe_title)
+          .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+          .setImage(data.img)
+          .setDescription(data.alt)
+        });
       } catch (err) {
         message.channel.send(err);
-      }*/
+      }
     });
   } else if (!isNaN(args[0])){
     xkcd(args[0], function (data) {
-
+      try {
+        message.channel.send({ embed: new Discord.RichEmbed()
+          .setAuthor('XKCD #' + data.num)
+          .setTitle(data.safe_title)
+          .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+          .setImage(data.img)
+          .setDescription(data.alt)
+        });
+      } catch (err) {
+        message.channel.send(err);
+      }
     });
   } else {
     message.reply("Please use +xkcd <number>");
