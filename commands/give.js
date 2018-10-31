921b323abfbc0cace7680a8fcbc7741327e35498
Hayley <hayl0830@gmail.com>
2018-10-31 22:54:17 +1100

    birb! for drew

--- commands/give.js
+++ commands/give.js
@@ -5,8 +5,9 @@ exports.run = (bot, message, args) => {
   let role = message.guild.roles.find("name", new_role);
 
   if(!message.member.hasPermission("MANAGE_ROLES")
- && !(message.author.id == '57506893358170112' && new_role == "Paramallamaccomplice")
- && !(message.author.id == '392492179748290561' && new_role == "kboobooboo bestie")
+    && !(message.author.id == '57506893358170112' && new_role == "Paramallamaccomplice")
+    && !(message.author.id == '392492179748290561' && new_role == "kboobooboo bestie")
+    && !(message.author.id == '281970530037268480' && new_role == "birb appreciators")
 )
     return message.reply( "No no no *YOU'RE* not allowed to do that! ");
 
