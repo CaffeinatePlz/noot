506425b137a5449c075ebe32fada72754f28e9f3
Hayley <hayl0830@gmail.com>
2019-05-08 21:33:25 +1000

    meow?

---
+++ commands/cat.js
@@ -0,0 +1,32 @@
+exports.run = (bot, message, args) => {
+    const Discord = require("discord.js");
+    const request = require('request-promise-native');
+    const cheerio = require('cheerio');
+
+    request.get('https://random.cat/', (err, res, page) => {
+        const $ = cheerio.load(page);
+        message.channel.send({ embed: new Discord.RichEmbed()
+                .setTitle(`Cat`)
+                .setColor([Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)])
+                .setImage($("#cat img").attr("src"))
+                .setURL(res.request.uri.href)
+        });
+    });
+
+
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    botPerms: [],
+    memberPerms: []
+};
+
+
+exports.help = {
+    name: 'cat',
+    description: 'posts a random cat',
+    usage: 'cat'
+};
